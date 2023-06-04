979fd0364548867abd231aa4d9a53f594f0d9eff
Luke <ljhorner2@gmail.com>
2023-06-04 16:02:26 +0100

    adding 2nd and 3rd maps

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let map;
 
 async function initMap() {
   // The location of Uluru
-  const position = { lat: -25.344, lng: 131.031 };
+  const position = { lat: 53.349940416635626, lng: -1.809374709678355 };
   // Request needed libraries.
   //@ts-ignore
   const { Map } = await google.maps.importLibrary("maps");
@@ -11,17 +11,67 @@ async function initMap() {
 
   // The map, centered at Uluru
   map = new Map(document.getElementById("map"), {
-    zoom: 4,
+    zoom: 14,
     center: position,
-    mapId: "DEMO_MAP_ID",
+    mapId: "Mam Tor",
   });
 
   // The marker, positioned at Uluru
   const marker = new AdvancedMarkerElement({
     map: map,
     position: position,
-    title: "Uluru",
+    title: "Mam Tor",
   });
 }
 
 initMap();
+
+//my second map
+async function initMap2() {
+    // The location of Castleton
+    const position = { lat:   53.34353163706478, lng: -1.7777066004976665 };
+    // Request needed libraries.
+    //@ts-ignore
+    const { Map } = await google.maps.importLibrary("maps");
+    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+  
+    // The map, centered at Uluru
+    map = new Map(document.getElementById("map2"), {
+      zoom: 14,
+      center: position,
+      mapId: "Castleton",
+    });
+  
+    // The marker, positioned at Uluru
+    const marker = new AdvancedMarkerElement({
+      map: map,
+      position: position,
+      title: "somewhere",
+    });
+  }
+  initMap2();
+
+  //my third map
+async function initMap3() {
+  // The location of Castleton
+  const position = { lat:   53.3989805001796, lng: -1.8764972519399676 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Uluru
+  map = new Map(document.getElementById("map3"), {
+    zoom: 14,
+    center: position,
+    mapId: "Kinder Downfall",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "Kinder Downfall",
+  });
+}
+initMap3();
